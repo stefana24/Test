49e033fc717dc9902895f05cb296a24f63d2165a
stefana24 <stefi_campean@yahoo.com>
2023-03-22 16:39:06 +0200

    another commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,6 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("this is a test");
+        System.out.println("please work");
     }
 }
